f9b85d8797f100cbcc0b8f80a67265f92c652816
David Turner <csusbdt@gmail.com>
2020-04-29 09:01:41 -0700

    X

--- public/a.js
+++ public/a.js
@@ -1,27 +1,64 @@
-console.log("(1) a.js is running");
-
 (function() {
-  function login() {
+  function sendEmailLoginLink() {
+    a_send_link_btn.disabled = true
     const email = a_email.value
-    // pretend we successfully logged in
-    a_msg.innerHTML            = "logged in"
-    a_logging_in.style.display = "none"
-    a_logged_in.style.display  = "block"
+    var actionCodeSettings = {
+      url             : location.href ,
+      handleCodeInApp : true
+    }
+    firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+    .then(function() {
+      a_msg.innerText = "Login link sent to email."
+      localStorage.setItem("email", email);
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
+  }
+
+  function loginPageLoad() {
+    let email = localStorage.getItem("email")
+    if (!email) {
+      email = prompt('Please provide your email for confirmation')
+    }
+    firebase.auth().signInWithEmailLink(email, location.href)
+    .then(function(result) {
+      localStorage.removeItem("email")
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
   }
 
   function logout() {
-    // pretend we successfully logged out
-    a_msg.innerHTML            = "logged out"
-    a_logging_in.style.display = "block"
-    a_logged_in.style.display  = "none"
+    a_logout_btn.disabled = true
+    firebase.auth().signOut().catch(function(error) {
+      console.log(error)
+    })
+  }
+
+  function authStateChanged(user) {
+    if (user === null) {
+      a_msg.innerHTML            = "logged out"
+      a_logging_in.style.display = "block"
+      a_logged_in.style.display  = "none"
+      a_send_link_btn.disable    = false
+    } else {
+      a_msg.innerHTML            = "logged in"
+      a_logging_in.style.display = "none"
+      a_logged_in.style.display  = "block"
+      a_logout_btn.disable       = false
+    }
   }
 
   document.addEventListener('DOMContentLoaded', () => {
-    console.log("(3) dom content loaded event fired")
     a_msg.innerHTML = "loaded"
-    a_logging_in.style.display = "block"
-    a_send_link_btn.addEventListener('click', login)
+    a_send_link_btn.addEventListener('click', sendEmailLoginLink)
     a_logout_btn.addEventListener('click', logout)
+    const auth = firebase.auth()
+    auth.onAuthStateChanged(authStateChanged)
+    if (auth.isSignInWithEmailLink(location.href)) {
+      loginPageLoad()
+    }
   })
 })();
-
