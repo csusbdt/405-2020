60329f296b4bdce3602f81aeb8a691da4d689e5c
David Turner <csusbdt@gmail.com>
2020-05-15 15:07:28 -0700

    X

--- public/b.js
+++ public/b.js
@@ -2,6 +2,16 @@
   a.login = function() {
     b_view.style.display = "block";
     b_view_edit_btn.disabled = false;
+    const userId            = firebase.auth().currentUser.uid
+    const userRef           = firebase.firestore().collection("users").doc(userId)
+    userRef.onSnapshot(function(snapshot) {
+      if (snapshot.exists) {
+        b_view_note.innerText = snapshot.data().note
+      } else {
+        userRef.set({ note: "my note" });
+      }
+      b_edit_note.value = b_view_note.innerText
+    })
   }
 
   a.logout = function() {
@@ -9,18 +19,21 @@
 
   document.addEventListener('DOMContentLoaded', () => {
     b_view_edit_btn.addEventListener('click', function() {
-      b_view_edit_btn.disabled = true
-      b_view.style.display = "none"
-      b_edit.style.display = "block"
-      b_edit_save_btn.disabled = false
+      b_view_edit_btn.disabled   = true
+      b_view.style.display       = "none"
+      b_edit.style.display       = "block"
+      b_edit_save_btn.disabled   = false
       b_edit_cancel_btn.disabled = false
     })
     b_edit_save_btn.addEventListener('click', function() {
-      b_view_edit_btn.disabled = false
-      b_view.style.display = "block"
-      b_edit.style.display = "none"
-      b_edit_save_btn.disabled = true
+      b_view_edit_btn.disabled   = false
+      b_view.style.display       = "block"
+      b_edit.style.display       = "none"
+      b_edit_save_btn.disabled   = true
       b_edit_cancel_btn.disabled = true
+      const userId               = firebase.auth().currentUser.uid
+      const userRef              = firebase.firestore().collection("users").doc(userId)
+      userRef.update({ note: b_edit_note.value })
     })
     b_edit_cancel_btn.addEventListener('click', function() {
       b_view_edit_btn.disabled = false
